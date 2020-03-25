96f5baf9242cd1898db32e116d93499294a614e1
naemulhasan <60178100+naemulhasan@users.noreply.github.com>
2020-03-25 11:14:13 -0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+</head>
+<body>
+	<script>
+		function myFun()
+		{
+			var correct_way= /^[A-Za-z]+$/;
+
+			var a = document.getElementById("User_Name").value;
+			if(a=="")
+			{
+				document.getElementById("Message").innerHTML="Enter a Valid email address";
+				return false;
+			}
+			
+			if(a.match(correct_way))
+				true;
+			else
+			{
+				document.getElementById("Message").innerHTML="use alphabets";
+				return false;
+			}
+		} 
+
+
+</script>
+<form onsubmit="return myFun()">
+
+Name:<br>
+<input type="text" id="User_Name" value=""> example@gmail.com
+<span id="Message"></span><br>
+<input type="submit"  value="Submit"></input>
+
+</form>
+</body>
+</html>
