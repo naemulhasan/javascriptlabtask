a4f52095ce7a56575e8620eff61837544e4118de
naemulhasan <60178100+naemulhasan@users.noreply.github.com>
2020-03-26 02:24:43 -0700

    Add files via upload

--- date.php
+++ date.php
@@ -1,39 +1,36 @@
 <html>
 <head>
 		<script>
-		function myFun()
-		{
-			
-
-			var a1 = document.getElementById("a1").value;
-			
-			if(a1.checked==true)
-			{
-				alert(" The selected one is :" +a1.value);
-				
-			}
-			
-			
-			else
-
-			alert(" The is no selected type:" );
-			
-		} 
-
+    function myFun(form) {
+        var a = document.getElementById("a1").value;
+        
+        if ( a=="") {
+           document.getElementById("Message").innerHTML="Enter The  date";
+				return false;
+        }
+        return true;
+        header("location:date.php");
+    }
 
 </script>
+
+
+
 
 </head>
 
 
-</body>
+<body>
 Select Date :<br>
 						
 							<input id="a1" type="Date" name="rdate">
 							<!-- <input type="text" size="2" name="date">/
 							<input type="text" size="2" name="month">/
 							<input type="text" size="2" name="year"> -->
-							</body><br>
+							<br>
 
 <button onclick="myFun()" id="btn1">Submit</button>
+<span id="Message"></span><br>
+</body>
+
 </html>
